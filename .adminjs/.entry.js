83e4ef7ff9bd13dc23524bd3aa33437b25ec86e3
Horace Wong <horaceyin@hotmail.com>
2024-01-30 14:11:41 +0800

    upload static resource

---
+++ .adminjs/.entry.js
@@ -0,0 +1,15 @@
+AdminJS.UserComponents = {}
+import LongField from '../dist/src/features/admin/components/LongField'
+AdminJS.UserComponents.LongField = LongField
+import PhotoField from '../dist/src/features/admin/components/PhotoField'
+AdminJS.UserComponents.PhotoField = PhotoField
+import AudioPlayButton from '../dist/src/features/admin/components/AudioPlayButton'
+AdminJS.UserComponents.AudioPlayButton = AudioPlayButton
+import ColumnCell from '../dist/src/features/admin/components/ColumnCell'
+AdminJS.UserComponents.ColumnCell = ColumnCell
+import GeneratePDF from '../dist/src/features/admin/components/GeneratePDF'
+AdminJS.UserComponents.GeneratePDF = GeneratePDF
+import Dashboard from '../dist/src/features/admin/components/Dashboard'
+AdminJS.UserComponents.Dashboard = Dashboard
+import PasswordEditComponent from '../node_modules/@adminjs/passwords/build/components/PasswordEditComponent'
+AdminJS.UserComponents.PasswordEditComponent = PasswordEditComponent
